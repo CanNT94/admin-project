d9ae69f650ddaca46616bae1a6d1c188825bb528
Can Nguyen <nguyenthiencan94@gmail.com>
2022-06-17 18:03:38 +0700

    Remove file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../ui/**/*.stories.mdx",
+    "../ui/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
