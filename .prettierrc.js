8c96d8b10f337ff0d82d34d71f8ef48c251cfe24
Can Nguyen <nguyenthiencan94@gmail.com>
2022-06-20 17:40:35 +0700

    Add husky pre commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 80,
+    endOfLine: 'auto',
+    arrowParens: 'avoid',
+    trailingComma: 'es5',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: true,
 };
