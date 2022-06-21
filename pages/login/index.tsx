f20474833ea32606d2fd3492697e8cb1d4220ef1
Can Nguyen <nguyenthiencan94@gmail.com>
2022-06-21 10:00:16 +0700

    Add Login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,21 +1,20 @@
 import Head from 'next/head';
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import Login from '../../components/login/Login';
 
 const LoginPage = () => {
-  return (
-    <Fragment>
-      <Head>
-        <title>Đăng ký/Đăng nhập</title>
-        <meta name="description" content="Đăng ký/Đăng nhập" />
-        <meta property="og:title" content="Đăng ký/Đăng nhập" />
-        <meta property="og:description" content="Đăng ký/Đăng nhập" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="vi_VN" />
-      </Head>
-      <Login />
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Head>
+                <title>Sign in</title>
+                <meta name="description" content="Sign in" />
+                <meta property="og:title" content="Sign in" />
+                <meta property="og:description" content="Sign in" />
+                <meta property="og:type" content="website" />
+            </Head>
+            <Login />
+        </Fragment>
+    );
 };
 
 export default LoginPage;
