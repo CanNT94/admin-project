e9e5edcb36580e92925ab5c22927e65d0a007de5
Can Nguyen <nguyenthiencan94@gmail.com>
2022-06-22 17:58:06 +0700

    Add input search component and menu button

--- components/ui/header/Header.tsx
+++ components/ui/header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { User } from '../../../model/user';
+import Input from '../input/input';
+import MenuButton from '../button/MenuButton';
 interface HeaderProps {
     user?: User;
 }
 
 export const Header = ({ user }: HeaderProps) => (
     <nav className="navbar fixed-top">
-        <div className="d-flex align-items-center navbar-left">Header left</div>
+        <div className="d-flex align-items-center navbar-left">
+            <MenuButton />
+            <Input placeholder="Search" type="text"></Input>
+        </div>
         <div className="navbar-logo">
             <span className="logo">Logo</span>
         </div>
