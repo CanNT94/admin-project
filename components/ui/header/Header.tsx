cfc924949a9f5e67f4f08d1f704cd071bb4e5b9d
Can Nguyen <nguyenthiencan94@gmail.com>
2022-06-23 15:02:19 +0700

    Add Logo

--- components/ui/header/Header.tsx
+++ components/ui/header/Header.tsx
@@ -2,24 +2,37 @@ import React from 'react';
 import { User } from '../../../model/user';
 import Input from '../input/input';
 import MenuButton from '../button/MenuButton';
+import Image from 'next/image';
+import logo from '../../../public/assets/images/smartosc_logo.png';
+import { useRouter } from 'next/router';
+
 interface HeaderProps {
     user?: User;
 }
 
-export const Header = ({ user }: HeaderProps) => (
-    <nav className="navbar fixed-top">
-        <div className="d-flex align-items-center navbar-left">
-            <MenuButton />
-            <Input
-                className="w-64"
-                placeholder="Search"
-                icon="bi bi-search"
-                type="text"
-            ></Input>
-        </div>
-        <div className="navbar-logo">
-            <span className="logo">Logo</span>
-        </div>
-        <div className="navbar-right">Header right</div>
-    </nav>
-);
+const Header = ({ user }: HeaderProps) => {
+    const router = useRouter();
+    return (
+        <nav className="navbar fixed-top">
+            <div className="d-flex align-items-center navbar-left">
+                <MenuButton />
+                <Input
+                    className="w-64"
+                    placeholder="Search"
+                    icon="bi bi-search"
+                    type="text"
+                ></Input>
+            </div>
+            <div
+                className="navbar-logo"
+                role="button"
+                onClick={() => router.push('/')}
+            >
+                <Image className="logo" src={logo} layout="fill"></Image>
+            </div>
+            <div className="navbar-right">Header right</div>
+        </nav>
+    );
+};
+
+export default Header;
