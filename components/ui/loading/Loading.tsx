7918522f6fa1502a696a8b418fc95d91ec0a640a
huytv04883 <huytv04883@gmail.com>
2022-07-01 17:51:04 +0700

    update masonry

---
+++ components/ui/loading/Loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="box-loading">
+            <div className="loader">
+                <span className="dot dot_1"></span>
+                <span className="dot dot_2"></span>
+                <span className="dot dot_3"></span>
+                <span className="dot dot_4"></span>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
