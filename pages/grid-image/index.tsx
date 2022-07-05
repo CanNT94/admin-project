202683273f309ef485cfb6ebeb5046f967ecb9f4
huytv04883 <huytv04883@gmail.com>
2022-07-05 16:06:01 +0700

    Update masonry

--- pages/grid-image/index.tsx
+++ pages/grid-image/index.tsx
@@ -13,15 +13,25 @@ const GridImagesPage = () => {
     const [dataImages, setDataImages] = useState<IImage[]>([]);
     const [pg, setPg] = useState<number>(1);
     const [isFetching, setIsFetching] = useState(false);
+    const [record, setRecord] = useState<number>(0);
+    const [col, setCol] = useState<number>(250);
+    const limit = 15;
     useLayoutEffect(() => {
-        fetch(`http://localhost:8888/masonry?_page=${pg}`, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then(res => res.json())
-            .then(data => {
+        const fetchDataMasonry = async () => {
+            try {
+                // If not set _limit default = 10
+                const res = await fetch(
+                    `http://localhost:8888/masonry?_page=${pg}&_limit=${limit}`
+                );
+                const data = await res.json();
+                const record = await res.headers.get('X-Total-Count');
                 setDataImages([...dataImages, ...data]);
-            });
+                setRecord(Number(record));
+            } catch (error) {
+                console.log('Error Fetch data:' + error);
+            }
+        };
+        fetchDataMasonry();
     }, [pg]);
 
     useEffect(() => {
@@ -50,6 +60,57 @@ const GridImagesPage = () => {
         }, 1000);
     };
 
+    const checkOverLoad = () => {
+        let flag = false;
+        const totalPage = Math.ceil(record / limit);
+        pg >= totalPage ? (flag = true) : (flag = false);
+        return flag;
+    };
+
+    const handleClick = (option: number) => {
+        switch (option) {
+            case 250:
+                setCol(option);
+                break;
+            case 300:
+                setCol(option);
+                break;
+            case 400:
+                setCol(option);
+                break;
+            case 500:
+                setCol(option);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const flag = checkOverLoad();
+
+    const masontyOptions = [
+        {
+            id: 1,
+            columnWidth: 250,
+            column: 'col-6',
+        },
+        {
+            id: 2,
+            columnWidth: 300,
+            column: 'col-5',
+        },
+        {
+            id: 3,
+            columnWidth: 400,
+            column: 'col-4',
+        },
+        {
+            id: 4,
+            columnWidth: 500,
+            column: 'col-3',
+        },
+    ];
+
     return (
         <>
             <Head>
@@ -59,12 +120,27 @@ const GridImagesPage = () => {
                 <meta property="og:description" content="Grid Images" />
                 <meta property="og:type" content="website" />
             </Head>
-            <Masonry gap={15}>
+            <div className="option-demo">
+                <ul>
+                    {masontyOptions.map(option => (
+                        <li
+                            key={option.id}
+                            className={`${
+                                col === option.columnWidth
+                                    ? 'active ' + option.column
+                                    : option.column
+                            }`}
+                            onClick={() => handleClick(option.columnWidth)}
+                        ></li>
+                    ))}
+                </ul>
+            </div>
+            <Masonry gap={15} columnWidth={col}>
                 {dataImages.map(data => {
                     return <Images key={data?.id} data={data} />;
                 })}
             </Masonry>
-            {isFetching === true && <Loading />}
+            {isFetching === true && flag === false && <Loading />}
         </>
     );
 };
