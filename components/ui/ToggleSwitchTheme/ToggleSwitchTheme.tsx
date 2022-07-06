034874b3bbe6b0d31d58976512927b6172f30e3d
Can Nguyen <nguyenthiencan94@gmail.com>
2022-07-06 13:46:05 +0700

    Fix default themes

--- components/ui/ToggleSwitchTheme/ToggleSwitchTheme.tsx
+++ components/ui/ToggleSwitchTheme/ToggleSwitchTheme.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTheme } from 'next-themes';
 
 const ToggleSwitchTheme = () => {
-    const { theme, setTheme } = useTheme();
-
+    const { systemTheme, theme, setTheme } = useTheme();
+    useEffect(() => {
+        if (!systemTheme) {
+            setTheme('light');
+        }
+    });
     return (
         <div className="btn-switch-theme mx-2">
             <button
                 id="tooltip_switch"
                 type="button"
-                className={theme !== 'dark' ? 'default' : 'dark-theme '}
+                className={theme === 'dark' ? 'dark-theme' : ''}
                 onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             >
                 <span className="switch-inner"></span>
